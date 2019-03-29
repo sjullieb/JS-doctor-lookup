7733ab5d7390f1b6324b8de3e5a1e1b8c8e0ed75
Yulia Shidlovskaya <sjullieb@gmail.com>
2019-03-29 15:21:27 -0700

    fill in the index.html

--- src/main.js
+++ src/main.js
@@ -4,5 +4,17 @@ import './styles.css';
 import $ from 'jquery';
 
 $(document).ready(function() {
+  
+  function showDoctor(data){
+    let htmlString = "<tr>";
+    htmlString += "<td>" + data.first_name + "</td>";
+    htmlString += "<td>" + data.last_name + "</td>";
+    htmlString += "<td>" + data.address + "</td>";
+    htmlString += "<td>" + data.phone_number + "</td>";
+    htmlString += "<td>" + data.website + "</td>";
+    htmlString += "<td>" + data.new_patients + "</td>";
+    htmlString += "</tr>";
+    $("#results").append(htmlString);
+  }
 
 });
