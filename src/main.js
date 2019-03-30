24088fe49085a6bd7e230703119ef66605d3651d
Guest User <Guest@epicodus-55.local>
2019-03-29 21:14:08 -0700

    add buttons Previous and Next

--- src/main.js
+++ src/main.js
@@ -10,73 +10,112 @@ import { Doctor, Practice } from './doctor.js';
 
 const lat = 47.6130071;
 const lon = -122.4121035;
+let pageNumber = 0;
+let perPage = 10;
+let previousNumberOnPage = 0;
+let name;
+let conditions;
 
 $(document).ready(function() {
 
+  $("#next").prop("disabled",true);
+  $("#previous").prop("disabled",true);
+
   $("#newSearch").submit(function(event){
     event.preventDefault();
-console.log(1);
-    let name = $("#name").val();
-    let conditions = $("#conditions").val();
+    name = $("#name").val();
+    conditions = $("#conditions").val();
+    pageNumber = 0;
+    previousNumberOnPage = perPage;
+
+    clearPreviousSearch();
 
     if ((name == "") && (conditions == "")) {
-      console.log("empty");
       alert("Please enter at least one search parameter!");
       $("#name").focus();
     }
     else {
-      console.log("call function");
       clearPreviousSearch();
-      let hasData = searchDoctors(lat, lon, name, conditions, showDoctors, showError);
-      console.log(hasData);
-      if (hasData){
-        $(".results").prepend("<h2>Search results</h2>");
-        console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-        $(".results").show();
-      }
-      else {
-        $(".noData").show();
-      }
+      searchDoctors(lat, lon, name, conditions, pageNumber, perPage, showDoctors, showError, changePreviousNumberOfDoctorsOnPage);
     }
   });
 
   function showDoctors(allDoctors){
     let htmlString = "";
-    for(let i=0; i < allDoctors.length; i++)
-    {
-      htmlString += "<div class='row'>";
-      htmlString += "<div class='col-md-4'><img src='" + allDoctors[i].imageUrl + "' alt='photo'></div>";
-      htmlString += "<div class='col-md-8'><h3>" + allDoctors[i].firstName + " " + allDoctors[i].lastName + "</h3>";
-      htmlString += "</div></div>";
+    clearPreviousSearch();
 
+    if (pageNumber == 0) {
+      $("#previous").prop("disabled",true);
+    }
+    else {
+      $("#previous").prop("disabled",false);
+    }
 
-      for(let k=0; k < allDoctors[i].practices.length; k++)
+    if (allDoctors.length == 0){
+      console.log("no data");
+      $(".noData").show();
+    }
+    else {
+      for(let i=0; i < allDoctors.length; i++)
       {
         htmlString += "<div class='row'>";
-        htmlString += "<div class='col-md-4'>";
-        htmlString += "<p>Practice name:<br>Address:<br>Phones:<br>Accepts new patients:</p>"
-        htmlString += "</div>";
+        htmlString += "<div class='col-md-4'><img src='" + allDoctors[i].imageUrl + "' alt='photo'></div>";
+        htmlString += "<div class='col-md-8'><h3>" + allDoctors[i].firstName + " " + allDoctors[i].lastName + "</h3>";
+        htmlString += "</div></div><br>";
 
-        htmlString += "<div class='col-md-8'>";
-        htmlString += "<p>" + allDoctors[i].practices[k].name + "<br>";
-        htmlString += allDoctors[i].practices[k].address + "<br>";
 
-        for(let j=0; j < allDoctors[i].practices[k].phones.length; j++)
+        for(let k=0; k < allDoctors[i].practices.length; k++)
         {
-          htmlString += allDoctors[i].practices[k].phones[j] + " ";
+          htmlString += "<div class='row'>";
+          htmlString += "<div class='col-md-4'>";
+          htmlString += "<p>Practice name:<br>Address:<br>Phones:<br>Accepts new patients:</p>"
+          htmlString += "</div>";
+
+          htmlString += "<div class='col-md-8'>";
+          htmlString += "<p>" + allDoctors[i].practices[k].name + "<br>";
+          htmlString += allDoctors[i].practices[k].address + "<br>";
+
+          for(let j=0; j < allDoctors[i].practices[k].phones.length; j++)
+          {
+            htmlString += allDoctors[i].practices[k].phones[j] + " ";
+          }
+          htmlString += "<br>";
+          htmlString += allDoctors[i].practices[k].newPatients + "<br>";
+          htmlString += "</p></div>"; // column
+          htmlString += "</div>"; // row
         }
-        htmlString += "<br>";
-        htmlString += allDoctors[i].practices[k].newPatients + "<br>";
-        htmlString += "</p></div>"; // column
-        htmlString += "</div>"; // row
+
+        htmlString += "<hr>";
+
       }
-
-      htmlString += "<hr>";
-
+      $(".results").append(htmlString);
+      $(".results").show();
     }
-    $(".results").append(htmlString);
-    $(".results").show();
   }
+
+  $("#next").click(function(){
+    if(previousNumberOnPage == perPage){
+      $("#previous").prop("disabled",false);
+      pageNumber++;
+      clearPreviousSearch();
+      searchDoctors(lat, lon, name, conditions, pageNumber, perPage, showDoctors, showError, changePreviousNumberOfDoctorsOnPage);
+    }
+  });
+
+  $("#previous").click(function(){
+    if(pageNumber != 0){
+      pageNumber--;
+      if (pageNumber == 0)
+      {
+        $("#previous").prop("disabled",true);
+      }
+      else {
+        $("#previous").prop("disabled",false);
+      }
+      clearPreviousSearch();
+      searchDoctors(lat, lon, name, conditions, pageNumber, perPage, showDoctors, showError, changePreviousNumberOfDoctorsOnPage);
+    }
+  });
 
   function clearPreviousSearch()
   {
@@ -89,5 +128,19 @@ console.log(1);
   function showError(error)
   {
     $(".errors").text(error);
+    $(".errors").show();
+  }
+
+  function changePreviousNumberOfDoctorsOnPage(newNumber)
+  {
+    if (newNumber < perPage)
+    {
+      $("#next").prop("disabled",true);
+    }
+    else {
+      $("#next").prop("disabled",false);
+    }
+
+    previousNumberOnPage = newNumber;
   }
 });
--- src/searchDoctors.js
+++ src/searchDoctors.js
@@ -5,21 +5,20 @@ export let allDoctors = [];
 
 //format for location: 47.6130071%2C-122.4121035%2C100 - Seattle
 
-export function searchDoctors(location_lat, location_lon, name, condition, showDoctors, showError){
+export function searchDoctors(location_lat, location_lon, name, condition, pageNumber, perPage, showDoctors, showError, changePreviousNumberOfDoctorsOnPage){
   //process.env.API_KEY
   let nameQuery = ``;
   let conditionQuery = ``;
+  let skip = pageNumber * perPage;
   if (name != "")
     nameQuery = `name=${name}&`;
   if (condition != "")
     conditionQuery = `query=${condition}&`;
-console.log(`https://api.betterdoctor.com/2016-03-01/doctors?`+ conditionQuery + nameQuery + `location=${location_lat}%2C${location_lon}%2C100&user_location=${location_lat}%2C${location_lon}&sort=full-name-asc&skip=0&limit=10&user_key=${process.env.apiKey}`);
-  $.get(`https://api.betterdoctor.com/2016-03-01/doctors?`+ conditionQuery + nameQuery + `location=${location_lat}%2C${location_lon}%2C100&user_location=${location_lat}%2C${location_lon}&sort=full-name-asc&skip=0&limit=10&user_key=${process.env.apiKey}`).then(function(response){
-
-console.log(response);
+console.log(`https://api.betterdoctor.com/2016-03-01/doctors?`+ conditionQuery + nameQuery + `location=${location_lat}%2C${location_lon}%2C100&user_location=${location_lat}%2C${location_lon}&skip=0&limit=10&user_key=${process.env.apiKey}`);
+  $.get(`https://api.betterdoctor.com/2016-03-01/doctors?`+ conditionQuery + nameQuery + `location=${location_lat}%2C${location_lon}%2C100&user_location=${location_lat}%2C${location_lon}&skip=${skip}&limit=${perPage}&user_key=${process.env.apiKey}`).then(function(response){
+      allDoctors = [];
       if(response.data.length > 0){
-        allDoctors = [];
-
+        changePreviousNumberOfDoctorsOnPage(response.data.length);
         let newDoctor;
         let address = "";
         let data;
@@ -27,15 +26,27 @@ console.log(response);
         {
           data = response.data[i];
           newDoctor = new Doctor(data.profile.last_name, data.profile.first_name, data.profile.image_url);
-          console.log(newDoctor);
           allDoctors.push(newDoctor);
           let newPractice;
           let address;
+          let newPatients;
           for(let k=0; k < data.practices.length; k++)
           {
             let address = data.practices[k].visit_address.street + " " +   data.practices[k].visit_address.city + " " + data.practices[k].visit_address.state + " " +
             data.practices[k].visit_address.zip;
-            newPractice = new Practice(data.practices[k].name, address, data.practices[k].accepts_new_patients);
+
+          //  console.log(data.practices[k].accepts_new_patients);
+
+            if(data.practices[k].accepts_new_patients.toString() == "true")
+            {
+              newPatients = "Yes";
+            }
+            else
+            {
+              newPatients = "No";
+            }
+            
+            newPractice = new Practice(data.practices[k].name, address, newPatients);
 
             for(let j=0; j < data.practices[k].phones.length; j++)
             {
@@ -45,12 +56,8 @@ console.log(response);
           }
         }
         console.log(allDoctors);
-        showDoctors(allDoctors);
-        return true;
       }
-      else {
-        return false;
-      }
+      showDoctors(allDoctors);
       //
 
     }).fail(function(error){
