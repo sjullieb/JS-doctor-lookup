620505d963d989012a40271a4a2d6a050337753a
Guest User <Guest@epicodus-55.local>
2019-03-29 17:55:42 -0700

    retrive information

---
+++ src/doctors.js
@@ -0,0 +1,39 @@
+import $ from 'jquery';
+
+//format for location: 47.6130071%2C-122.4121035%2C100 - Seattle
+
+export function searchDoctors(location_lat, location_lon, name, condition, showDoctor, showError){
+  //process.env.API_KEY
+  let nameQuery = ``;
+  let conditionQuery = ``;
+  if (name != "")
+    nameQuery = `name=${name}&`;
+  if (condition != "")
+    conditionQuery = `query=${condition}&`;
+console.log(`https://api.betterdoctor.com/2016-03-01/doctors?`+ conditionQuery + nameQuery + `location=${location_lat}%2C${location_lon}%2C100&user_location=${location_lat}%2C${location_lon}&sort=full-name-asc&skip=0&limit=10&user_key=${process.env.apiKey}`);
+  $.get(`https://api.betterdoctor.com/2016-03-01/doctors?`+ conditionQuery + nameQuery + `location=${location_lat}%2C${location_lon}%2C100&user_location=${location_lat}%2C${location_lon}&sort=full-name-asc&skip=0&limit=10&user_key=${process.env.apiKey}`).then(function(response){
+
+console.log(response);
+      if(response.data.length > 0){
+      //   let address = "";
+      //   for(let i=0; i < response.data.length; i++){
+      //     let address = response.data[i].practices[0].visit_address.street + " " +   response.data[i].practices[0].visit_address.city + " " + response.data[i].practices[0].visit_address.state + " " +
+      //     response.data[i].practices[0].visit_address.zip;
+      //
+      //     let phone = "";
+      //     for(let j=0; j < response.data[i].practices[0].phones.length; j++)
+      //       phone += response.data[i].practices[0].phones[j].number + " ";
+      //     showDoctor(response.data[i].profile.last_name, response.data[i].profile.first_name,
+      //       phone, address, response.data[i].practices[0].accept_new_patients);
+      //   }
+        return true;
+      }
+      else {
+        return false;
+      }
+      //
+
+    }).fail(function(error){
+      showError(error);
+    });
+}
--- src/main.js
+++ src/main.js
@@ -2,19 +2,62 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from 'jquery';
+import { searchDoctors } from './doctors.js';
+
+// Seattle location
+// 47.6130071 -122.4121035
+
+const lat = 47.6130071;
+const lon = -122.4121035;
 
 $(document).ready(function() {
-  
-  function showDoctor(data){
+
+  $("#newSearch").submit(function(event){
+    event.preventDefault();
+console.log(1);
+    let name = $("#name").val();
+    let conditions = $("#conditions").val();
+
+    if ((name == "") && (conditions == "")) {
+      console.log("empty");
+      alert("Please enter at least one search parameter!");
+      $("#name").focus();
+    }
+    else {
+      console.log("call function");
+      clearPreviousSearch();
+      let hasData = searchDoctors(lat, lon, name, conditions, showDoctor, showError);
+      if (hasData){
+        $(".results").show();
+      }
+      else {
+        $(".noData").show();
+      }
+    }
+  });
+
+  function showDoctor(lastName, firstName, address, phoneNumber, website, newPatients){
     let htmlString = "<tr>";
-    htmlString += "<td>" + data.first_name + "</td>";
-    htmlString += "<td>" + data.last_name + "</td>";
-    htmlString += "<td>" + data.address + "</td>";
-    htmlString += "<td>" + data.phone_number + "</td>";
-    htmlString += "<td>" + data.website + "</td>";
-    htmlString += "<td>" + data.new_patients + "</td>";
+    htmlString += "<th>" + lastName + "</th>";
+    htmlString += "<th>" + firstName + "</th>";
+    htmlString += "<th>" + address + "</th>";
+    htmlString += "<th>" + phoneNumber + "</th>";
+    htmlString += "<th>" + website + "</th>";
+    htmlString += "<th>" + newPatients + "</th>";
     htmlString += "</tr>";
-    $("#results").append(htmlString);
+    $(".doctor").append(htmlString);
   }
 
+  function clearPreviousSearch()
+  {
+    $(".results").empty();
+    $(".results").hide();
+    $(".errors").hide();
+    $(".noData").hide();
+  }
+
+  function showError(error)
+  {
+    $(".errors").text(error);
+  }
 });
